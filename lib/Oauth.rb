b70aac74d80a7de2f3940cb2e7129de0589780b0
Ethan Collins <49600380+ethancollins0@users.noreply.github.com>
2019-11-05 13:42:32 -0700

    Update Oauth.rb

--- lib/Oauth.rb
+++ lib/Oauth.rb
@@ -8,7 +8,7 @@ require 'json'
     def self.access_token
         token = RestClient.post('https://accounts.spotify.com/api/token',
             {'grant_type': 'client_credentials'},
-            {'Authorization': ''})
+            {'Authorization': 'Basic YzhjZTljY2NiMDczNDg2YmE5OTJkNGUyOTM0NzBhNzA6N2ZmYTRlY2Q4Yjk1NDQ2OGJlMTI2ZGZiZDY1MGU1NWM='})
         JSON.parse(token)['access_token']
     end
  end
